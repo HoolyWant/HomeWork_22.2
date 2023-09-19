8dea626922c77aedbe99458c74181743c2b86cad
hoolywant <sir.saltickow@yandex.ru>
2023-09-19 21:59:01 +0400

    1-3 tasks are done

--- shop/urls.py
+++ shop/urls.py
@@ -1,3 +1,5 @@
+from django.views.decorators.cache import cache_page
+
 from .apps import ShopConfig
 from django.conf import settings
 from django.conf.urls.static import static
@@ -15,7 +17,7 @@ urlpatterns = [
     path('<int:pk>', ProductView.as_view(), name='product_info'),
     path('create/', BlogCreateView.as_view(), name='create_blog'),
     path('blog/', BlogListView.as_view(), name='blog_list'),
-    path('blog/<int:pk>', BlogDetailView.as_view(), name='blog_view'),
+    path('blog/<int:pk>', cache_page(60)(BlogDetailView.as_view()), name='blog_view'),
     path('edit/<int:pk>', BlogUpdateView.as_view(), name='blog_edit'),
     path('delete/<int:pk>', BlogDeleteView.as_view(), name='blog_delete'),
     path('create_product/', ProductCreate.as_view(), name='product_create'),
